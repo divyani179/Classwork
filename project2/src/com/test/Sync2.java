fd2ef86bd4016b3aabc8366827580dcf3f1457a3
dsain2 <dsain2@BLRSRW39.sapient.com>
2015-02-03 15:19:18 +0530

    jo

---
+++ project2/src/com/test/Sync2.java
@@ -0,0 +1,58 @@
+package com.test;
+class Runthread implements Runnable{
+	Thread t;
+	Loan l;
+	int account ;
+	String name;
+	public Runthread(Loan lo, int account, String name) {
+		
+		 lo= l;
+		this.account = account;
+		this.name = name;
+		t=new Thread();
+		t.start();
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("hello"+name);
+		double in=l.calcloan(account);
+		System.out.println("your interest is"+in);
+		}
+	}
+public class Sync2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("welcome to the net banking");
+   
+   Loan l=new Loan();
+   Runthread s1=new  Runthread(l,100,"div");
+   Runthread s2=new  Runthread(l,200,"shiv");
+   Runthread s3=new  Runthread(l,678,"sam");
+   try{
+	   s1.t.join();
+	   s2.t.join();
+	   s3.t.join();
+	   
+   }catch(InterruptedException e){
+	   e.printStackTrace();
+   }
+   System.out.println("in main at last");
+   
+	}
+
+}
+class Loan{
+	double bal=4000;
+	double calcloan(int amount){
+		double res=0;
+		try{
+			bal=bal-amount;
+			Thread.sleep(1000);
+		}catch(InterruptedException e){
+			System.out.println(e);
+		}
+		return bal;
+	}
+}
